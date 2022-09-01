84d86bae27ab1d832727d5485bad50e76a688577
Kusumavathi <kusuma192001@gmail.com>
2022-09-01 16:56:24 +0530

    commit

--- spark/spark.py
+++ spark/spark.py
@@ -1,12 +1,19 @@
 from  pyspark.sql import SparkSession
+import os
+
+
+os.environ["SPARK_HOME"] = "C:\\Users\\Kusuma\\spark-3.2.2-bin-hadoop3.2"
+os.environ["HADOOP_HOME"] ="C:\\Users\\Kusuma\\spark-3.2.2-bin-hadoop3.2\\bin\\hadoop"
+os.environ["PATH"] += ";C:\\Users\Kusuma\\spark-3.2.2-bin-hadoop3.2\\bin\hadoop\\bin"
 
 spark = SparkSession\
 .builder\
 .appName("Spark SQL basic example")\
 .getOrCreate()
-
-
 df=spark.read.option('header','true').csv('export.csv')
 df.show()
-#df.write.option('compression','snappy').parquet('export2.parquet')
-df.printSchema()
+df.write.mode("overwrite").option('compression','snappy').parquet('export1.parquet')
+#df.printSchema()
+
+# Select only the "name" column
+#df.select("email").show()
